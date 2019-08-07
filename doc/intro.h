3d58a9c20984a5ee7f79fe3859bc504ea390ab07
John Hu <huziyong@gmail.com>
2019-08-07 22:28:57 +0800

    Add intro.h for doxygen mainpage

---
+++ doc/intro.h
@@ -0,0 +1,42 @@
+/* This file is a dummy header which is used to generate the
+ * introduction in Doxygen. */
+
+/**
+ * @mainpage RETHINK C --- C Algorithms Library
+ * 
+ * @section Introduction
+ * 
+ * Relearn and rethink C Programming Language, including some of data structures and algorithms.
+ * 
+ * Some of the code inspired (copied) by Simon Howard's [c-algorithms](https://github.com/fragglet/c-algorithms), like ArrayList, etc. This project also reused his alloc-testing framework for memory testing.
+ * 
+ * RETHINK-C aims to build a reuseable codebase for C Programming Language. 
+ * 
+ * @section Data_structures Data structures
+ * 
+ * @subsection Basic Data Structures
+ * 
+ * @li @link arraylist.h ArrayList @endlink: Automatically resizing array.
+ * @li @link list.h LinkedList @endlink: LinkedList.
+ * @li @link queue.h Queue @endlink: Queue.
+ * @li @link bitmap.h BitMap @endlink: BitMap.
+ * @li @link matrix.h Muti-dimensional Matrix @endlink: Muti-dimensional Matrix.
+ * @li @link hash_table.h Hash Table @endlink: Hash Table.
+ * 
+ * @subsection Trees
+ * 
+ * @li @link bstree.h Binary Search Tree @endlink: Binary Search Tree.
+ * @li @link avltree.h AVL Tree @endlink: AVL Tree.
+ * @li @link rbtree.h Red Black Tree @endlink: Red Black Tree.
+ * @li @link heap.h Binary Heap @endlink: Binary Heap.
+ * 
+ * @subsection Graphs
+ * 
+ * @li @link graph.h Adjacency Matrix @endlink: Adjacency Matrix.
+ * @li @link dijkstra.h Dijkstra @endlink: Dijkstra.
+ * 
+ * @subsection Graphs
+ * 
+ * @li @link bignum.h BigNum integer @endlink: BigNum integer.
+ * 
+ */
